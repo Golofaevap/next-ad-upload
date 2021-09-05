9227fb5e6b857f03b38dfe2934d296e42f3003ae
Marina <golofaeva.xs4n@gmail.com>
2021-09-05 18:24:14 +0800

    path lib added

--- pages/api/post.js
+++ pages/api/post.js
@@ -1,7 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 const fs = require("fs");
+const path = require("path");
+
 export default function handler(req, res) {
-    let functionLog = fs.readFileSync("./scriptTemplates/logger.js", "utf-8");
+    const root = "./";
+    const logFuncPath = "scriptTemplates/logger.js";
+    const strPath = path.join(root, logFuncPath);
+    let functionLog = fs.readFileSync(path.resolve(strPath), "utf-8");
 
     res.status(200).json({ ok: true, result: functionLog });
 }
